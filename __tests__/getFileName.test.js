4403871d7d5682724772df44f0612ff1070302bb
rustam-amirov <r.amirov@yahoo.com>
2022-06-15 23:09:09 +0300

    getFileName

--- __tests__/getFileName.test.js
+++ __tests__/getFileName.test.js
@@ -1,7 +1,23 @@
 import getFileName from '../src/getFileName.js';
 import { test, expect } from '@jest/globals';
 
-test('right_url', () => {
-    const fileName = getFileName('https://ru.hexlet.io/courses');
-    expect(fileName).toEqual('ru-hexlet-io-courses.html');
+const url = "https://ru.test.com";
+test('url html', () => {
+    const fileName = getFileName('https://ru.test.com/courses', url);
+    expect(fileName).toEqual('ru-test-com-courses.html');
+});
+
+test('url js', () => {
+    const fileName = getFileName('https://ru.test.com/packs/js/runtime.js', url);
+    expect(fileName).toEqual('ru-test-com-packs-js-runtime.js');
+});
+
+test('path', () => {
+    const fileName = getFileName('/courses', url);
+    expect(fileName).toEqual('ru-test-com-courses.html');
+});
+
+test('path2', () => {
+    const fileName = getFileName('/assets/application.css', url);
+    expect(fileName).toEqual('ru-test-com-assets-application.css');
 });
--- src/getFileName.js
+++ src/getFileName.js
@@ -1,6 +1,19 @@
-export default (url, end = '.html') => {
-    const newUrl =  new URL(url.trim());
-    const path = (newUrl.pathname.trim().length > 1) ? newUrl.pathname.trim() : '';
+import path from 'path';
+
+export default (url, mainUrl, end = '') => {
+    const dir = path.parse(url);
+    let ext;
+    if (dir.ext.length < 1 && end.length < 1) {
+       ext = '.html';
+    } else if (end.length >= 1) {
+        ext = end;
+    } else {
+        ext = dir.ext;
+    }
+    const mainPath = dir.dir === '/' ? dir.dir + dir.name : dir.dir + '/' + dir.name;
+
+    const newUrl =  new URL(mainPath, mainUrl);
+    const newpath = (newUrl.pathname.trim().length > 1) ? newUrl.pathname.trim() : '';
     const host = newUrl.host.trim();
-    return (host+path).replace(/[^a-z0-9\s]/gi, '-')+end;
+    return (host+newpath).replace(/[^a-z0-9\s]/gi, '-')+ext;
 }
