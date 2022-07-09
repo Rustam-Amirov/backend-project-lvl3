8c190219e86dd26bdfefcfc82e838df4a27db15e
rustam-amirov <r.amirov@yahoo.com>
2022-07-09 17:56:12 +0300

    refactoring getLinks

--- src/getLinks.js
+++ src/getLinks.js
@@ -2,25 +2,25 @@ import * as cheerio from 'cheerio';
 import _ from 'lodash';
 export default (html, url) => {
     const $ = cheerio.load(html);
+    const searchElements = {
+        img: 'src',
+        link: 'href',
+        script: 'src'
+    };
     const links = [];
     $('img, link, script').each((index, element) => {
-        if ($(element).attr('src') !== undefined) {
-            links[index] = $(element).attr('src');
-        } else if ($(element).attr('href') !== undefined) {
-            links[index] = $(element).attr('href');
+        const currentTag = $(element).prop("tagName").toLowerCase();
+        if ($(element).attr(searchElements[currentTag]) !== undefined) {
+            links[index] = $(element).attr(searchElements[currentTag]);
         }
     });
+
     return links.filter((link) => {
         if( _.startsWith(link, 'http')) {
             const currentLink = new URL(link);
             return currentLink.origin === url;
-        } else if  (_.startsWith(link, '/')) {
-            const currentLink = new URL(link, url);
-            return currentLink.origin === url;
         } else {
-            const fullUrl = url + '/' + link;
-            const currentLink = new URL(fullUrl);
-            return currentLink.origin === url;
+            return true;
         }
     });
 }
